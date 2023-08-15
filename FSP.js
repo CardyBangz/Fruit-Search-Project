2bb3d620823154e13f8a613b635ce77848d661f3
CardyBangz <133674601+CardyBangz@users.noreply.github.com>
2023-08-15 19:36:54 -0400

    Add files via upload

---
+++ FSP.js
@@ -0,0 +1,40 @@
+const inputBox = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+const dropdown=document.querySelector('#dropdown')
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	let results = [];
+    const fruitSearch= inputBox.value.toLowerCase();
+    const fruitFilter= fruit.filter(fruit=> fruit.toLowerCase().includes(fruitSearch))
+
+	return results;
+}
+
+function searchHandler(e) {
+    
+	// TODO
+}
+
+
+
+function showSuggestions(results, inputVal) {
+    dropwdown.innerHTML="";
+    for(i=0;i<o,i++){
+        suggestions.value=results[i];
+        dropdown.appendChild(suggestions);
+    }
+
+	// TODO
+}
+
+function useSuggestion(e) {
+	input.Use{
+}
+return fruitFilter
+};
+showSuggestions(fruitFilter);
+
+const Keys=inputBox.addEventListener('keyup', searchHandler);
+const Use=suggestions.addEventListener('click', useSuggestion); 
